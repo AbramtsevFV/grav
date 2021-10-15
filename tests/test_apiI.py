fdd597020ebffbbef1076c4ecb4db84956c8a4a3
AbramtsevFV <donfedor007@mail.ru>
2021-10-15 16:30:30 +0300

    fix testst func test_API

--- tests/test_apiI.py
+++ tests/test_apiI.py
@@ -14,8 +14,19 @@ def test_bad_request():
     assert utils.get_req('k@ya.ru') == {'message': 'The user with the email k@ya.ru  was not found'}
 
 
-@pytest.mark.parametrize('a', [0, 1, 2, 3, 4, 5, 6, 7])
-def test_API(result, email, a):
-    res = client.get(f'/grav?q={email[a]}')
+@pytest.mark.parametrize('a, email',
+                         [
+                             (0, 'k.bx@ya.ru'),
+                             (1, 'eehzntm5@hotmail.com'),
+                             (2, 'manuruel@yahoo.com'),
+                             (3, 'russellebb912@hotmail.com'),
+                             (4, 'jnkitchener@btinternet.com'),
+                             (5, 'ras-nie@web.de'),
+                             (6, 'ghagen4@gmail.com'),
+                             (7, 'mattdhoey@gmail.com')
+                         ]
+                         )
+def test_API(result, a, email):
+    res = client.get(f'/grav?q={email}')
     assert res.status_code == 200
     assert json.loads(res.data) == result[a]
